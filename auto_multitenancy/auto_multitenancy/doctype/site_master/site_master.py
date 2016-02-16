31de48944ea0e47c43e65236b4009575886cc949
shitolepriya <priya.s@indictranstech.com>
2016-02-16 19:12:39 +0530

    [set default pwds]

--- auto_multitenancy/auto_multitenancy/doctype/site_master/site_master.py
+++ auto_multitenancy/auto_multitenancy/doctype/site_master/site_master.py
@@ -5,6 +5,13 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import throw, msgprint, _
 
 class Sitemaster(Document):
-	pass
+	# pass
+
+	def validate(self):
+		if self.get("__islocal") :
+			domain = str(self.domain)
+			if domain[-13:] != "vlinkuerp.com" :
+				frappe.throw(_("Damain name should be 'vlinkuerp.com', Please enter correct domain..!! "))
