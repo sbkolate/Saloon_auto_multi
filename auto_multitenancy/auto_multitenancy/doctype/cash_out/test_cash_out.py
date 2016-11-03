dad8d918e8bb521397eb6559293daaa70d76479e
Gangadhar <gangadhar.k@indictranstech.com>
2016-11-03 18:17:32 +0530

    added cash out

---
+++ auto_multitenancy/auto_multitenancy/doctype/cash_out/test_cash_out.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, New Indictrans Technologies pvt. ltd. and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+# test_records = frappe.get_test_records('Cash Out')
+
+class TestCashOut(unittest.TestCase):
+	pass
